021f6f32dbe0e188057480a3377b84a53ff3c645
axim-soft <94515531+axim-soft@users.noreply.github.com>
2021-12-06 11:12:18 +0530

    Update sbenergy_build_train_pipeline.py

--- ml_service/pipelines/sbenergy_build_train_pipeline.py
+++ ml_service/pipelines/sbenergy_build_train_pipeline.py
@@ -62,6 +62,12 @@ def main():
 
         # Use a CSV to read in the data set.
         file_name = "juno_3month_one_hour.csv"
+        
+        if not os.path.exists("run_train_pipeline.py"):
+            raise Exception(
+                'Could not find py at "%s". If you have bootstrapped your project, you will need to provide a CSV.'  # NOQA: E501
+                % file_name
+            )
 
         if not os.path.exists(file_name):
             raise Exception(
